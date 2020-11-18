b9338139efba61f651284626e62ad1e75dfd6558
fm16191 <56755856+fm16191@users.noreply.github.com>
2020-11-18 12:57:06 -1000

    J7 : ex8 str_beautifuler (tests à la con)

--- j7_ex8_str_beautifuler.php
+++ j7_ex8_str_beautifuler.php
@@ -1,28 +1,43 @@
 <?php
 
-function str_beautifuler($str){
-  while(strpos($str, '  ')){ // Si y'a plusieurs espaces consécutifs alors récursivement remplacer un double espace par un espace simple (+ d'un espace -> 1 espace)
-    $str = str_replace("  ", " ", $str);
-  }
-  $str = trim($str); // supprimer les espaces en début et en fin de string
-  // echo $str;
-
-  // $l = array();
-  $str = strtolower($str); //Met $str en full minuscules
-  $str .= " ";
-  // echo $str;
-
-  for($i=0; $i<strlen($str)-1; $i++){
-    if($str[$i+1] == " "){
-      $str[$i] = strtoupper($str[$i]);
+function str_beautifuler(...$arguments){
+  try{
+    if (func_num_args() < 1){ // si pas d'arguments return NULL
+      return NULL;
     }
+    $str = func_get_arg(0); // récupérer l'argument7
+    if(!is_string($str) or strlen($str) == 0){
+      return NULL;
+    }
+    while(!is_bool(strpos($str, "  ")) or !is_bool(strpos($str, "\t"))){ // Si y'a plusieurs espaces consécutifs alors récursivement remplacer un double espace par un espace simple (+ d'un espace -> 1 espace)
+      $str = str_replace("  ", " ", $str);
+      $str = str_replace("\t", " ", $str);
+    }
+    $str = trim($str); // supprimer les espaces en début et en fin de string
+    // echo $str;
+
+    // $l = array();
+    $str = strtolower($str); //Met $str en full minuscules
+    $str .= " "; //Ajouter un espace à la fin du mot pour pouvoir faire le if dans le for juste après
+    // echo $str;
+
+    for($i=0; $i<strlen($str)-1; $i++){
+      if($str[$i+1] == " "){ // si il y a un espace juste après (donc si c'est la fin du mot), mettre la dernière lettre ($str[$i]) en majuscule
+        $str[$i] = strtoupper($str[$i]);
+      }
+    }
+    return trim($str); // supprime l'espace de fin
+    // echo $str;
+  } catch (Exception $e){
+    return NULL;
   }
-  return trim($str); // supprime l'espace de fin
-  // echo $str;
 
 }
 
 
-echo str_beautifuler(" Un PanGolin    cAcHe  ");
+var_dump(str_beautifuler("          Un \tPangOLin        caCHe    "));
+var_dump(str_beautifuler());
+var_dump(str_beautifuler(0));
+var_dump(str_beautifuler("aaaaaa"));
 
 ?>
